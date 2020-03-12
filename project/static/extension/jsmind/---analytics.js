0b9c4f4793218e9764d8f7c4b4d6c584477c0f23
mizxc <xiangxianjiao@163.com>
2020-03-12 12:34:36 +0800

    V2.5

--- project/static/extension/jsmind/---analytics.js
+++
@@ -1,6 +0,0 @@
-var _Hasync= [['Histats.start', '1,3295792,4,0,0,0,00010000'], ['Histats.fasi', '1'], ['Histats.track_hits', '']];
-(function() {
-    var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-    hs.src = ('http://s10.histats.com/js15_as.js');
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();
